4dae39898cc7775f930a817e02a634f7306781a3
dik522 <mara.vojta@post.cz>
2025-07-27 15:00:48 +0200

    Changed version in setup scripts. Added .gitignore file.

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@ npm init -y
 npm install ascii-table@0.0.9 axios@1.7.7 cheerio@1.0.0-rc.12 discord.js@14.14.1 diagnostics_channel@1.1.0 dotenv@16.4.5 express@4.19.2 mongodb@6.8.0
 $json = Get-Content package.json | ConvertFrom-Json
 $json.type = "module"
-$json.version = "1.0.0"
+$json.version = "1.0.1"
 $json.main = "src/index.js"
 $json.author = "Dik522"
 $json | ConvertTo-Json -Depth 10 | Set-Content package.json
